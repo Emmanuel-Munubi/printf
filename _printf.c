b011eb86e8f616b25ff2742a4ce52e39c6736089
Emmanuel-Munubi <manumunubi@gmail.com>
2023-01-24 14:11:08 +0300

    added _printf

--- _printf.c
+++ _printf.c
@@ -1,50 +1,22 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
 /**
- * State
- * 0 = normal
- * 1 = escape
+ * _printf - prints a string
+ * @format: string
+ * @ap: va_list
+ *
+ * Return: string
  */
-void my_print(const char *fmt, va_list ap)
-{
-	int state = 0;
-
-	while (*fmt)
-	{
-		if (state == 0)
-		{
-			putchar(*fmt);
-			if (*fmt == '%')
-				state = 1;
-		} else if (state == 1)
-		{
-			t_print specifiers[] = {
-				{'c', &print_character},
-				{'s', &print_string},
-				{(char)NULL, NULL}
-			};
-
-			for(int i = 0; specifiers[i].f; i++)
-			{
-				if(*fmt++ == specifiers[i].f)
-				{
-					specifiers[i].func(ap);
-				}
-			}
-			state = 0;
-		}
-		fmt++;
-	}
-}
-
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
+	va_list ap;
+	va_start(ap, format);
 
-	my_print(format, args);
+	return (my_print(format, ap));
 
-	va_end(args);
-
+	va_end(ap);
 }
