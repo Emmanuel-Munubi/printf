3f1dafd0c6f0468b3cec44fea5ded74960657b34
Emmanuel-Munubi <manumunubi@gmail.com>
2023-01-26 15:38:20 +0300

    betty check

--- test/my_printer.c
+++ test/my_printer.c
@@ -12,16 +12,22 @@ int my_print(const char *fmt, va_list ap, char *buffer, char *specifier_buffer)
 	int state = 0;
 	int i = 0;
 	int count = 0;
-	while (fmt[i] != '\0') {
-		if (state == 0) {
-			if (fmt[i] == '%') {
+
+	while (fmt[i] != '\0')
+	{
+		if (state == 0)
+		{
+			if (fmt[i] == '%')
+			{
 				state = 1;
-			} else {
+			} else
+			{
 				/*putchar(fmt[i]);*/
 				buffer[count] = fmt[i];
 				count++;
 			}
-		} else if (state == 1) {
+		} else if (state == 1)
+		{
 			specifier_buffer = get_specifier_buffer(fmt[i], ap);
 			_strcat(buffer, specifier_buffer);
 			count += _strlen(specifier_buffer);
@@ -30,5 +36,5 @@ int my_print(const char *fmt, va_list ap, char *buffer, char *specifier_buffer)
 		i++;
 	}
 	buffer[count] = '\0';
-	return count;
+	return (count);
 }
