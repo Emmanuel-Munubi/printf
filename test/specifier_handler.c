06db855a18812baf463ad308fb0e9a92bbf4b10b
Emmanuel-Munubi <manumunubi@gmail.com>
2023-01-27 09:29:30 +0300

    betty check

--- test/specifier_handler.c
+++ test/specifier_handler.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * get_print_func - gets the apropriate printing function
- * for a given format-specifier
+ * get_specifier_buff - gets the specifier buffer by calling
+ * the mapped function
  * @c: format-specifier
+ * @ap: va_list
  *
  * Return: string or empty string
  */
-char *get_specifier_buffer(char c, va_list ap)
+char *get_specifier_buff(char c, va_list ap)
 {
 	int i;
 	t_print t[] = {
@@ -16,15 +17,16 @@ char *get_specifier_buffer(char c, va_list ap)
 	    {'u', print_unsigned_int},
 	    {'d', print_int},
 	    {'i', print_int},
-	    /*// {'o', print_octal},
-	    // {'x', print_hex},
-	    // {'X', print_hex},
-	    // {'b', print_binary},
-	    // {'S', print_big_s},
-	    // {'p', print_pointer},
-	    // {'r', print_rev},
-	    //  {'R', print_rot},*/
-	    {'\0', NULL}};
+	    {'o', print_octal},
+	    {'x', print_hex},
+	    {'X', print_hex},
+	    {'b', print_binary},
+	    {'S', print_big_s},
+	    {'p', print_pointer},
+	    {'r', print_rev},
+	    {'R', print_rot},
+	    {'\0', NULL}
+	};
 	for (i = 0; t[i].f; i++)
 	{
 		if (t[i].f == c)
